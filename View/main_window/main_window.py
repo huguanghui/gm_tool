5644dadd31f70b4f27d7ab1081b846ebb2b3a9cb
huguanghui <huguanghui1971@gmail.com>
2023-10-23 08:58:48 +0800

    更新处理逻辑

--- View/main_window/main_window.py
+++ View/main_window/main_window.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import sys
 import ctypes
+import time
 
 from common import resource
 from common.config import config
@@ -19,6 +20,15 @@ from View.setting_interface import SettingInterface
 from .ui_main import Ui_MainWindow
 
 
+class TiHeader(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ("crc", ctypes.c_uint32),
+        ("len", ctypes.c_uint16),
+        ("type", ctypes.c_uint8),
+    ]
+
+
 class TiFileInfo(ctypes.Structure):
     _fields_ = [
         ("btime", ctypes.c_uint32),
@@ -28,6 +38,14 @@ class TiFileInfo(ctypes.Structure):
         ("tags", ctypes.c_uint32),
         ("size", ctypes.c_uint32),
         ("res", ctypes.c_uint32 * 3)
+    ]
+
+
+class TiSlice(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ("header", TiHeader),
+        ("file_info", TiFileInfo)
     ]
 
 
@@ -114,18 +132,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.read_file(file_name[0])
 
     def read_file(self, file_path):
+        item_cnt = 0
+        offset_sz = 0
         with open(file_path, 'rb') as file:
-            # 读取文件数据
-            data = file.read(32)
-            print(data)
-            # 将数据转换为TiFileInfo结构体对象
-            file_info = TiFileInfo.from_buffer_copy(data)
-            # 打印结构体成员
-            print("btime:", file_info.btime)
-            print("btime_ms:", file_info.btime_ms)
-            print("channel:", file_info.channel)
-            print("etime:", file_info.etime)
-            print("tags:", file_info.tags)
-            print("size:", file_info.size)
-            # struct_size = ctypes.sizeof(TiFileInfo)
-            # print("Struct size:", struct_size)
+            try:
+                while True:
+                    # 读取文件数据
+                    item_size = ctypes.sizeof(TiSlice)
+                    print(f"offset_sz: {offset_sz}")
+                    data = file.read(item_size)
+                    if not data:
+                        break
+                    # 将数据转换为TiFileInfo结构体对象
+                    slice = TiSlice.from_buffer_copy(data)
+                    b_local_time = time.localtime(slice.file_info.btime)  # 将时间戳转换为本地时间的结构化时间对象
+                    b_time_str = time.strftime("%Y-%m-%d %H:%M:%S", b_local_time)  # 将结构化时间对象转换为字符串
+                    e_local_time = time.localtime(slice.file_info.etime)  # 将时间戳转换为本地时间的结构化时间对象
+                    e_time_str = time.strftime("%Y-%m-%d %H:%M:%S", e_local_time)  # 将结构化时间对象转换为字符串
+                    file_size = slice.file_info.size / (1025 * 1024)  # 文件大小（MB）
+                    formatted_size = "{:.2f}".format(file_size)  # 保留两位小数
+                    print(f"btime: {b_time_str} etime: {e_time_str} size: {formatted_size} MB type: {slice.header.type}")
+                    print(f"item_cnt: {item_cnt}")
+                    item_cnt += 1
+                    offset_sz += item_size
+            except IOError as e:
+                print(f"Error reading file: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            print(f"item_cnt: {item_cnt}")
